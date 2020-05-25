abf5dbb3e12b9fcadac1d8a4812824da4f4265d8
John Freitas <john.freitasau@gmail.com>
2020-05-25 18:08:04 +1000

    Added csv files upload function

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -116,8 +116,11 @@ const Dashboard: React.FC = () => {
               {transactions.map(transaction => (
                 <tr key={transaction.id}>
                   <td className="title">{transaction.title}</td>
-                  <td className="income">{transaction.formattedValue}</td>
-                  <td>{transaction.type}</td>
+                  <td className={transaction.type}>
+                    {transaction.type === 'outcome' && '- '}
+                    {transaction.formattedValue}
+                  </td>
+                  <td>{transaction.category.title}</td>
                   <td>{transaction.formattedDate}</td>
                 </tr>
               ))}
